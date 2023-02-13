fc1cfc1719fc9da66ca00d63a1fda9833d5ae556
user <alisheroverzhigit857@gmail.com>
2023-02-13 18:16:03 +0600

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+
+        public static final double PI = 3.14;
+
+        public static double area(double radius) {
+            return (PI * (radius * radius));
+        }
+
+        public static double crtg (double radius) {
+            return (PI * 2 * radius);
+        }
+    }
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+
+        Circle.area(5);
+        System.out.println(Circle.area(5));
+        System.out.println(Circle.crtg(5));
+    }
+}
